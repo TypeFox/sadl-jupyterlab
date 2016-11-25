25f5083a0d03ce144a4f3c8f43e85ddb89d53b18
Sven Efftinge <sven.efftinge@typefox.io>
2016-11-25 15:16:49 +0100

    fixed editorservice

--- sadl-client/src/languageServer/editorService/plugin.ts
+++ sadl-client/src/languageServer/editorService/plugin.ts
@@ -14,8 +14,6 @@ import {
     defaultPathResolver, PathHandler, UriHandler
 } from '../../monaco/uris';
 
-import JLabHandler from './uriHandler';
-
 export const editorServiceProvider: JupyterLabPlugin<IEditorService> = {
     id: 'sadl.extensions.languageServer.editorService',
     requires: [
@@ -29,6 +27,5 @@ export const editorServiceProvider: JupyterLabPlugin<IEditorService> = {
 function activateEditorServices(app: JupyterLab, serviceManager: IServiceManager): IEditorService {
     const pathHandler = new PathHandler(app, serviceManager);
     const uriHandler = new UriHandler(defaultPathResolver, pathHandler);
-    const sadlUriHandler = new JLabHandler(uriHandler);
-    return new JupyterLabEditorService(sadlUriHandler);
+    return new JupyterLabEditorService(uriHandler);
 }
